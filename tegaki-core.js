b1eb6d8350f434712e19aa3efa6f16cd85ca1cb5
toshinka <129437032+toshinka@users.noreply.github.com>
2025-06-11 21:24:06 +0900

    Update tegaki-core.js

--- tegaki-core.js
+++ tegaki-core.js
@@ -17,7 +17,17 @@ javascript:(() => {
   closeButton.style.top = '5px';
   closeButton.style.right = '5px';
   closeButton.style.zIndex = '2000';
-  closeButton.addEventListener('click', () => canvas.remove());
+  closeButton.addEventListener('click', () => {
+    const targetCanvas = document.getElementById('oejs');
+    if (targetCanvas) {
+      const targetCtx = targetCanvas.getContext('2d');
+      targetCanvas.width = canvas.width;
+      targetCanvas.height = canvas.height;
+      targetCtx.drawImage(canvas, 0, 0);
+    }
+    canvas.remove();
+    closeButton.remove();
+  });
   canvas.parentNode.insertBefore(closeButton, canvas.nextSibling);
 
   let isDrawing = false;
