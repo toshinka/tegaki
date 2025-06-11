d089ffa1d0e0541ca0d9e692c958dea612f6e552
toshinka <129437032+toshinka@users.noreply.github.com>
2025-06-11 22:29:05 +0900

    Update tegaki-core.js

--- tegaki-core.js
+++ tegaki-core.js
@@ -5,7 +5,7 @@ javascript:(() => {
   canvas.style.position = 'fixed';
   canvas.style.top = '0';
   canvas.style.left = '0';
-  canvas.style.zIndex = '2000000021'; // FUTAKUROを上回る
+  canvas.style.zIndex = '2000000021';
   document.body.appendChild(canvas);
   const ctx = canvas.getContext('2d');
   ctx.fillStyle = 'white';
@@ -16,4 +16,22 @@ javascript:(() => {
   closeButton.style.position = 'absolute';
   closeButton.style.top = '5px';
   closeButton.style.right = '5px';
-  closeButton.style.zIndex = '2000000022'; //
+  closeButton.style.zIndex = '2000000022';
+  closeButton.addEventListener('click', () => canvas.remove());
+  canvas.parentNode.insertBefore(closeButton, canvas.nextSibling);
+
+  let isDrawing = false;
+  canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    ctx.beginPath();
+    ctx.moveTo(e.offsetX, e.offsetY);
+  });
+  canvas.addEventListener('mousemove', (e) => {
+    if (isDrawing) {
+      ctx.lineTo(e.offsetX, e.offsetY);
+      ctx.stroke();
+    }
+  });
+  canvas.addEventListener('mouseup', () => isDrawing = false);
+  canvas.addEventListener('mouseleave', () => isDrawing = false);
+})();
