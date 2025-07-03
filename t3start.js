2f3dd3f90c68d87a162c340c1ef565496ece310c
toshinka <129437032+toshinka@users.noreply.github.com>
2025-07-03 10:10:23 +0900

    Update t3start.js

--- t3start.js
+++ t3start.js
@@ -6,7 +6,7 @@ javascript:((async (d) => {
      */
 
     try {
-        const toolUrl = 'https://toshinka.github.io/tegaki/Phase3B/ToshinkaTegakiTool.html';
+        const toolUrl = 'https://toshinka.github.io/tegaki/Phase4A'7/ToshinkaTegakiTool.html';
         const response = await fetch(toolUrl);
         if (!response.ok) {
             throw new Error(`HTMLの読み込みに失敗しました: ${response.status} ${response.statusText}`);
