116e70e6611edb1f52fa5ede1643e1f356a706ff
toshinka <129437032+toshinka@users.noreply.github.com>
2025-08-12 10:03:56 +0900

    update

--- etc/Phase4A11E-4R/vite-project/src/AppBootstrap.js
+++
@@ -1,7 +0,0 @@
-import { AppBootstrap } from './app/AppController.js'; //変更: AppController -> AppBootstrap
-
-// Wait for the document to be fully loaded before starting the app
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("🚀 Application starting...");
-    new AppBootstrap(); //変更: AppController -> AppBootstrap
-});
